c419ec43f1472befb4fa5d69700d9452e81e4643
agnieszka <agnieszka.gos.1987@com>
2023-07-20 22:34:55 +0200

    Food menu component

---
+++ src/components/FoodMenu/FoodMenu.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import './foodMenu.scss';
+
+const ApiUrl = "http://localhost:3001/menu";
+
+const FoodMenu = () => {
+    const [menu, setMenu] = useState([]);
+
+    useEffect(() => {
+        menuGrid();
+    }, []);
+
+    const menuGrid = async () => {
+        const response = await fetch(ApiUrl); // Removed quotes around ApiUrl
+        const jsonData = await response.json();
+        setMenu(jsonData);
+    };
+
+    return (
+        <div className="container">
+            <h2>Menu Naszej Kawiarni</h2>
+            <div className="flex">
+            {menu.map((values) => (
+                <div className="container__box" key={values.id}>
+                    <div className="container__box-content">
+                        <div className="container__box-text">
+                            <h3>{values.name}</h3>
+                            <h4>{values.category}</h4>
+                            <p>{values.description}</p>
+                        </div>
+                        <p className="container__box-text">{values.price}</p>
+                    </div>
+                </div>
+            ))}
+        </div>
+        </div>
+    );
+};
+
+export default FoodMenu;
