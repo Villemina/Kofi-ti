72bd70f5b7dc66b0792a4e17d3bd9f92786df488
agnieszka <agnieszka.gos.1987@com>
2023-07-20 22:35:33 +0200

    Food menu component update

--- src/pages/CaffeMenu/CaffeMenu.js
+++ src/pages/CaffeMenu/CaffeMenu.js
@@ -1,12 +1,14 @@
 import Footer from "../../components/Footer/Footer";
 import Menu from "../../components/Menu/Menu";
 import MenuWrapper from "../../components/MenuWrapper/MenuWrapper";
+import FoodMenu from "../../components/FoodMenu/FoodMenu";
 
 const CaffeMenu = () => {
     return (
         <div>
             <Menu/>
             <MenuWrapper/>
+            <FoodMenu/>
             <Footer/>
         </div>
     );
