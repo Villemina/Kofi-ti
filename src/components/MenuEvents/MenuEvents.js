083916a7e2fb1e8395445c7b3bf1f3bd9e4a13c5
agnieszka <agnieszka.gos.1987@com>
2023-07-21 23:04:20 +0200

    Menu Bootstrap Styling

--- src/components/MenuEvents/MenuEvents.js
+++ src/components/MenuEvents/MenuEvents.js
@@ -1,18 +1,18 @@
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from "react-bootstrap/Container";
-
+import './MenuEvents.css'
 
 
 const MenuEvents = () => {
     return (
-        <Navbar bg="success" expand="lg" className="z-1 navbar-light   bg-opacity-0.5 ">
-            <Container>
-                <Navbar.Brand href="/">Kofi&ti</Navbar.Brand>
+        <Navbar expand="lg" >
+            <Container className="flex">
+                <Navbar.Brand href="/" className="brand">Kofi&ti</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto ">
-                        <Nav.Link href="/"> Strona Główna
+                <Navbar.Collapse id="basic-navbar-nav" >
+                    <Nav className="ms-auto ">
+                        <Nav.Link href="/" className="nav-link"> Strona Główna
                         </Nav.Link>
                         <Nav.Link href="/menu"> Menu
                        </Nav.Link>
